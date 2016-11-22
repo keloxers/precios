8ba382a8544c168d4c21c5174700f98e8270647a
Miguel Mendez <keloxers@gmail.com>
2016-11-22 11:35:55 -0300

    go

--- app/routes.php
+++ app/routes.php
@@ -97,15 +97,16 @@ Route::resource('articulos', 'ArticulosController');
 
 		Route::post('/api/precios', function () {
 
-			$pprecio = Input::get('precio', 10);
+			// $pprecio = Input::get('precio', 10);
 
 			// $articulo = $_POST['articulo'];
 			// $precio = $_POST['precio'];
 			try {
 
-							$precio = new Precio;
-							$precio->precio =  $pprecio;
-							$precio->save();
+							// $precio = new Precio;
+							// $precio->precio =  $pprecio;
+							// $precio->save();
+							return;
 
 						}
 
